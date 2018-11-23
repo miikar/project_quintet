5bce40aa473aeed33b80ba6b8280d78f83265a32
Antti Kurkinen <antti.kurkinen@struu.com>
2018-11-24 00:55:52 +0200

    change db collection to profiles

--- backend/schema.js
+++ backend/schema.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-
 const profileSchema = new mongoose.Schema({
   name: String,
   description: String
 });
 
-const ProfileSchema = mongoose.model('profileSchema', profileSchema);
+const ProfileSchema = mongoose.model('profiles', profileSchema);
 
-module.exports = { ProfileSchema }; 
+module.exports = { ProfileSchema };
