d14b743871cc45be33f43c18bba04fa9fdfb3181
Miika Rouvinen <miika.j.rouvinen@gmail.com>
2018-11-24 12:45:15 +0200

    ml experiments

--- backend/ml_algo.py
+++ backend/ml_algo.py
@@ -3,36 +3,51 @@ import pandas as pd
 import sklearn
 
 import pymongo
-
+import sys
+import json
 from pymongo import MongoClient
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 # from surprise import 
 
 
+#Read data from stdin
+def read_in():
+    lines = sys.stdin.readlines()
+    #Since our input would only be having one line, parse our JSON data from that
+    return json.loads(lines[0])
+
 def main():
 
+    # db connection
     client = MongoClient('mongodb://localhost:27017/')
     db = client['test']
-    collection = db['profileschemas']
+    collection = db['profiles']
 
-    cursor = collection.find({})
+    #get our data as an array from read_in()
+    #profile_id = read_in()
 
-    for doc in cursor:
-        print(doc)
+    profiles = collection.find({})
+    desc_data = {}
 
+    for profile in profiles:
+        desc_data[profile['name']] = profile['description']
 
-    corpus = [
-        'This is the first document.',
-        'This document is the second document.',
-        'And this is the third one.',
-        'Is this the first document?',
-    ]
-
+    y = desc_data.keys()
     # extract tf-idf features
-    vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(corpus)
-    print(vectorizer.get_feature_names())
+    X = TfidfVectorizer().fit_transform(desc_data.values())
+    cosine_similarities = linear_kernel(X[0:1], X).flatten()
+    ids = cosine_similarities.argsort()[:-5:-1]
+    
+    
     print(X.shape)
+    print(cosine_similarities.shape)
+    print(cosine_similarities)
+
+    print(desc_data[y[ids[0]]])
+    print(y[ids[0]])
+    print(desc_data[y[ids[1]]])
+    print(y[ids[1]])
 
 
 if __name__ == "__main__":
