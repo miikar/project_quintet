40523c5d82868c785a0b72e9655943f5c06551d9
Antti Kurkinen <antti.kurkinen@struu.com>
2018-11-24 20:13:23 +0200

    add socket io client and server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Login from './Login/Login';
 import ListView from './List/ListView';
+import openSocket from 'socket.io-client';
 import './bootstrap/bootstrap.css';
 import './App.css';
 import Nav from './Nav';
 
 class App extends Component {
+  componentDidMount() {
+    const socket = openSocket('http://cloud.kurkinen.me:3001');
+    setTimeout(() => {
+      socket.emit('message', 'test message');
+    }, 1000);
+  }
 
   render() {
     return (
