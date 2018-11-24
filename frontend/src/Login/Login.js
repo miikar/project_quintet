c15f217fdb9fd235a7db61de59a37ffb020b9818
Antti Kurkinen <antti.kurkinen@struu.com>
2018-11-24 19:24:59 +0200

    create profile on upload

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -4,14 +4,46 @@ import { uploadVideo } from '../Api';
 import { format } from 'util';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { profileName: '', profileDescription: '' };
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeDescription = this.handleChangeDescription.bind(this);
+  }
+  handleChangeName(event) {
+    this.setState({ profileName: event.target.value });
+  }
+  handleChangeDescription(event) {
+    this.setState({ profileDescription: event.target.value });
+  }
   onDrop = (accepted, rejected) => {
-    const profileId = '5bf918241a2d1345241006af'; //Todo post profile first and then upload video
-    uploadVideo(profileId, accepted);
+    const profile = {
+      name: this.state.profileName,
+      description: this.state.profileDescription
+    };
+    uploadVideo(profile, accepted);
   };
   render() {
     return (
       <div>
         <h2>Login</h2>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.profileName}
+            onChange={this.handleChangeName}
+          />
+        </label>
+        <label>
+          Name:
+          <input
+            type="textarea"
+            value={this.state.profileDescription}
+            onChange={this.handleChangeDescription}
+          />
+        </label>
         <Dropzone onDrop={this.onDrop} accept="video/mp4,video/x-m4v,video/*" />
       </div>
     );
