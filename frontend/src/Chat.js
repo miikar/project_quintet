4af7ba58cfb815d9f497f760f3761b51269b0949
joope <pyry.joona@helsinki.fi>
2018-11-25 00:06:07 +0200

    Chat improvements

--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -8,7 +8,7 @@ class Chat extends Component {
 
   state = {
     message: '',
-    messages : [{id: 0, content:'Message'}],
+    messages : [{id: 0, content:'Say hello!'}],
   }
 
   componentDidMount() {
@@ -43,14 +43,35 @@ class Chat extends Component {
       <div className="profile-chat">
         <div className="message-container">
           {messages.map((m) => (
-            <div className={"message type-"+m.id}>
-              {m.content}
+            <div className={"message message-type-"+m.id}>
+              {m.id === 1 && <span className="message-name">Them</span>}
+              <span className="message-content">
+                {m.content}
+              </span>
+              {m.id === 0 && <span className="message-name">You</span>}
             </div>
           ))}
         </div>
-        <form onSubmit={this.sendMessage}>
-          <input type="text" value={message} onChange={this.handleMessageInput} />
-          <button onClick={this.sendMessage} type="submit" value="Submit">Painike </button>
+        <form className="message-input" onSubmit={this.sendMessage}>
+          <div className="input-group mb-3">
+            <input type="text" 
+              autoFocus
+              className="form-control" 
+              placeholder="" 
+              aria-label="..." 
+              aria-describedby="basic-addon2"
+              value={message} 
+              onChange={this.handleMessageInput}
+            />
+            <div className="input-group-append">
+              <button 
+                className="btn btn-outline-primary"
+                onClick={this.sendMessage} 
+                type="submit"
+                 value="Submit">Send
+              </button>
+            </div>
+          </div>
         </form>
       </div>
     );
