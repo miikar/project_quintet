38fe9996636b789d5d9095655ab10d25480e6c45
Antti Kurkinen <antti.kurkinen@struu.com>
2018-11-25 11:40:17 +0200

    update token

--- backend/insertdata.js
+++ backend/insertdata.js
@@ -29,13 +29,17 @@ const main = async () => {
     {
       headers: {
         Authorization:
-          'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjY1ZjRhZmFjNjExMjlmMTBjOTk5MTU1ZmE1ODZkZWU2MGE3MTM3MmIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZGlnaXRhbC1wbGF0Zm9ybS1wcm9kIiwicGljdHVyZSI6Ii9pbWcvYXZhdGFyLmpwZyIsImF1ZCI6ImRpZ2l0YWwtcGxhdGZvcm0tcHJvZCIsImF1dGhfdGltZSI6MTU0Mjk3OTk2NiwidXNlcl9pZCI6IlJJVE9VNGFrYWhNNGpCcXJKak4wSU1MSjZNeDEiLCJzdWIiOiJSSVRPVTRha2FoTTRqQnFySmpOMElNTEo2TXgxIiwiaWF0IjoxNTQzMTMwOTU5LCJleHAiOjE1NDMxMzQ1NTksImVtYWlsIjoiYW50dGlrdXIrbGF1bmNocGFkQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImFudHRpa3VyK2xhdW5jaHBhZEBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.GuBawkPh7BMYwupkJ-aInMM2WelIHAYsDI1-o7vpvCBkC4crZrokPGSHCp8JbXnxCWOQdeEOIBkGttb84zcBY5sTY_y5DSFKZOkS9iMlCbJQ6GBe_vEmE8EycrXvg0AXllb_gLMxyffmsqRTz5K_kP5bVhx7gplfJGwEWzOucqbl4QzqrqZfHKz7A9mR9wbOaINrymqEZnyDRd6-r564hS93FVtiNBXhBJ19xyKqIKdBfYap5B30tPhNISOQT4RuS70npn2XFb3NX6MzFiFb8IxGcpwLcAOo4D12sanIbDSNhsJzuuigmSX3fEqKM46xZQbVW1FqpXWM3gQaRaugHA'
+          'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjY1ZjRhZmFjNjExMjlmMTBjOTk5MTU1ZmE1ODZkZWU2MGE3MTM3MmIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZGlnaXRhbC1wbGF0Zm9ybS1wcm9kIiwicGljdHVyZSI6Ii9pbWcvYXZhdGFyLmpwZyIsImF1ZCI6ImRpZ2l0YWwtcGxhdGZvcm0tcHJvZCIsImF1dGhfdGltZSI6MTU0Mjk3OTk2NiwidXNlcl9pZCI6IlJJVE9VNGFrYWhNNGpCcXJKak4wSU1MSjZNeDEiLCJzdWIiOiJSSVRPVTRha2FoTTRqQnFySmpOMElNTEo2TXgxIiwiaWF0IjoxNTQzMTM4NDE4LCJleHAiOjE1NDMxNDIwMTgsImVtYWlsIjoiYW50dGlrdXIrbGF1bmNocGFkQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImFudHRpa3VyK2xhdW5jaHBhZEBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.kni7EzUa_vsCSIit3bgHM3GbmhVEJ-RfIfQ2BogAqVMV633GTBV1CUy1u4-iX5W0oQnz9jg2cyrhZLafNU8qO1GF4jQcOg-FjPp-2SwbAVUtuIgYv0dtPE51_lof65Q8WFV3tw46xpIl3S9hYIjt7jIuGQbYBwqzo3O-xXSBDg8TCiVnwXvohjYQaPDXkeY3sdnsFbwakQipthxGB8iYu__x-G1zGw0CuNPFkYy573JHB3XinRl-i-BPA95K5Os_rx-9iH51HSwi_axMllKI6udmcoCmGz2mM0QPQQT78t6ThXC06XS2bYtxEtU1PMnYKEvf8zmODueT8VEPO_wusA'
       }
     }
   ).catch(error => {
     console.log('api err', error);
-  });
-  await ProfileSchema.collection.drop();
+    });
+  try {
+    await ProfileSchema.collection.drop();
+  } catch (ex) {
+    console.log('ex', ex);
+  }
   const insertData = data.body.items.map(s => {
     return {
       name: s.name,
